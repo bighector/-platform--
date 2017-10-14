45a6ab00d8ab18aeec07772e4ea076cde7c1ca47
李鹏军 <939961241@qq.commkdir>
2017-10-14 14:06:03 +0800

    添加登陆验证码

--- platform-shiro/src/main/java/com/platform/controller/SysLoginController.java
+++ platform-shiro/src/main/java/com/platform/controller/SysLoginController.java
@@ -56,10 +56,10 @@ public class SysLoginController {
     @ResponseBody
     @RequestMapping(value = "/sys/login", method = RequestMethod.POST)
     public R login(String username, String password, String captcha) throws IOException {
-//		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
-//		if(!captcha.equalsIgnoreCase(kaptcha)){
-//			return R.error("验证码不正确");
-//		}
+        String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
+        if (!captcha.equalsIgnoreCase(kaptcha)) {
+            return R.error("验证码不正确");
+        }
 
         try {
             Subject subject = ShiroUtils.getSubject();
