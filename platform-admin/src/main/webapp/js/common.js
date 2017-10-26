7ed2de9bbc6a3e80077676b62c1324878701b1e9
李鹏军 <939961241@qq.commkdir>
2017-10-26 23:37:28 +0800

    alert使用

--- platform-admin/src/main/webapp/js/common.js
+++ platform-admin/src/main/webapp/js/common.js
@@ -71,13 +71,13 @@ function getSelectedRow() {
     var grid = $("#jqGrid");
     var rowKey = grid.getGridParam("selrow");
     if (!rowKey) {
-        alert("请选择一条记录");
+        iview.Message.error("请选择一条记录");
         return;
     }
 
     var selectedIDs = grid.getGridParam("selarrrow");
     if (selectedIDs.length > 1) {
-        alert("只能选择一条记录");
+        iview.Message.error("只能选择一条记录");
         return;
     }
 
@@ -89,7 +89,7 @@ function getSelectedRows() {
     var grid = $("#jqGrid");
     var rowKey = grid.getGridParam("selrow");
     if (!rowKey) {
-        alert("请选择一条记录");
+        iview.Message.error("请选择一条记录");
         return;
     }
     return grid.getGridParam("selarrrow");
