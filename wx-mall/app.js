d47bb26df62eac2e7ccf7792b2b755bb0b8aff16
elfertower <m-27@sina.com>
2018-01-22 17:09:04 +0800

    未登录情况下 小程序 用户昵称显示 项目构建时支持UTF-8 字符集

--- wx-mall/app.js
+++ wx-mall/app.js
@@ -16,7 +16,7 @@ App({
   
   globalData: {
     userInfo: {
-      nickname: 'Hi,游客',
+      nickName: 'Hi,游客',
       username: '点击去登录',
       avatarUrl: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
     },
