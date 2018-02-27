1e0e6bb56b4646fba31737b4898c1d041bb46176
elfertower <m-27@sina.com>
2018-02-27 11:59:58 +0800

    增加：分类的级联删除

--- platform-shop/src/main/java/com/platform/service/impl/CategoryServiceImpl.java
+++ platform-shop/src/main/java/com/platform/service/impl/CategoryServiceImpl.java
@@ -5,6 +5,7 @@ import com.platform.entity.CategoryEntity;
 import com.platform.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -52,7 +53,9 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
+    @Transactional
     public int deleteBatch(Integer[]ids) {
+        categoryDao.deleteByParentBatch(ids);
         return categoryDao.deleteBatch(ids);
     }
 }
