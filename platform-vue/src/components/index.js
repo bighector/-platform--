475137a7d0035b38c6e4f238fb68cfb7be7be693
Neho <daniel.deng.haibiao@gmail.com>
2018-03-05 19:11:19 +0800

    1,添加cell组件，2，添加cell-group组件，3，添加订单确认页面

--- platform-vue/src/components/index.js
+++ platform-vue/src/components/index.js
@@ -6,6 +6,8 @@ import number from './packages/number/index.js'
 import selector from './packages/selector/index.js'
 import specialCell from './packages/special-cell/index.js'
 import goodsBox from './packages/goods-box/index.js'
+import cell from './packages/cell/index.js'
+import cellGroup from './packages/cell-group/index.js'
 
 const version = '1.0.0'
 
@@ -19,6 +21,8 @@ const install = function (Vue, config = {}) {
     Vue.component(selector.name, selector)
     Vue.component(specialCell.name, specialCell)
     Vue.component(goodsBox.name, goodsBox)
+    Vue.component(cell.name, cell)
+    Vue.component(cellGroup.name, cellGroup)
 };
 
 export default {
